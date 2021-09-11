f8e186a2332cecdcb6ff5d2f8cefc483a7c79c4c
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2021-09-12 01:01:06 +0530

    Update About Us Fragment

--- app/src/main/java/com/scriptech/vstudy/AboutUs.java
+++ app/src/main/java/com/scriptech/vstudy/AboutUs.java
@@ -59,28 +59,28 @@ public class AboutUs extends Fragment {
 
         founder1_n = v.findViewById(R.id.card_founder_1).findViewById(R.id.team_mem_name);
         founder2_n = v.findViewById(R.id.card_founder_2).findViewById(R.id.team_mem_name);
-        cofounder1_n = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_name);
+      //  cofounder1_n = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_name);
 
         founder1_i = v.findViewById(R.id.card_founder_1).findViewById(R.id.team_mem_img);
         founder2_i = v.findViewById(R.id.card_founder_2).findViewById(R.id.team_mem_img);
-        cofounder1_i = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img);
+      //  cofounder1_i = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img);
 
         founder1_linkedin = v.findViewById(R.id.card_founder_1).findViewById(R.id.team_mem_img_linkedin);
         founder2_linkedin = v.findViewById(R.id.card_founder_2).findViewById(R.id.team_mem_img_linkedin);
-        cofounder1_linkedin = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img_linkedin);
+      //  cofounder1_linkedin = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img_linkedin);
         founder1_insta = v.findViewById(R.id.card_founder_1).findViewById(R.id.team_mem_img_insta);
         founder2_insta = v.findViewById(R.id.card_founder_2).findViewById(R.id.team_mem_img_insta);
-        cofounder1_insta = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img_insta);
+     //   cofounder1_insta = v.findViewById(R.id.card_cofounder_1).findViewById(R.id.team_mem_img_insta);
 
         founder1_n.setText("Hitesh Sukhwani");
         founder2_n.setText("Saurav Singh");
-        cofounder1_n.setText("Pratham Vaidya");
+      //  cofounder1_n.setText("Pratham Vaidya");
 
         founder1_i.setImageResource(R.drawable.hitesh);
         founder2_i.setImageResource(R.drawable.saurav);
-        cofounder1_i.setImageResource(R.drawable.pratham);
+     //   cofounder1_i.setImageResource(R.drawable.pratham);
 
-        JoinTeam = v.findViewById(R.id.join_team);
+      //  JoinTeam = v.findViewById(R.id.join_team);
 
         founder1_linkedin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -92,12 +92,12 @@ public class AboutUs extends Fragment {
                 clicked("https://www.linkedin.com/in/saurav-singh-6bb8a31b2");
             }
         });
-        cofounder1_linkedin.setOnClickListener(new View.OnClickListener() {
+      /*  cofounder1_linkedin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 clicked("https://www.linkedin.com/in/pratham-vaidya-a196a8183");
             }
         });
-
+*/
         founder1_insta.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 clicked("https://instagram.com/sukhwanihitesh_?utm_medium=copy_link");
@@ -108,13 +108,13 @@ public class AboutUs extends Fragment {
                 clicked("https://instagram.com/saurav_singh_rathore_?utm_medium=copy_link");
             }
         });
-        cofounder1_insta.setOnClickListener(new View.OnClickListener() {
+        /*cofounder1_insta.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 clicked("https://instagram.com/prathamvaidya?utm_medium=copy_link");
             }
-        });
+        });*/
 
-        JoinTeam.setOnClickListener(new View.OnClickListener() {
+        /*JoinTeam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment fragment = new webview();
@@ -128,10 +128,10 @@ public class AboutUs extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+*/
 
-
-        contributorsRV = v.findViewById(R.id.rv_contributors);
-        contributorsArrayList = new ArrayList<>();
+       // contributorsRV = v.findViewById(R.id.rv_contributors);
+       /* contributorsArrayList = new ArrayList<>();
         contributorsRV.setHasFixedSize(true);
         contributorsRV.setLayoutManager(new GridLayoutManager(getActivity(), 3));
         contributorsRVadapter = new contributorsAdapter(contributorsArrayList, getActivity());
@@ -160,7 +160,7 @@ public class AboutUs extends Fragment {
             public void onFailure(@NonNull Exception e) {
                 Toast.makeText(getActivity(), "Fail to get the data.", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
 
         return v;
