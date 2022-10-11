097eb487cd0da43c6e0c16ccc42d3eac82ce0374
Muskan Sukhwani <muskansukhwani21@gmail.com>
2022-10-11 13:21:56 +0530

    Create new books adapter

--- app/src/main/java/com/scriptech/vstudy/ui/fragments/home/Home.kt
+++ app/src/main/java/com/scriptech/vstudy/ui/fragments/home/Home.kt
@@ -3,13 +3,14 @@ package com.scriptech.vstudy.ui.fragments.home
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.scriptech.vstudy.adapters.bookAdapter
+import com.scriptech.vstudy.adapters.BooksAdapter
 import com.scriptech.vstudy.adapters.videoAdapter
 import com.scriptech.vstudy.database.NotesDatabase
 import com.scriptech.vstudy.databinding.FragHomeBinding
@@ -23,7 +24,7 @@ class Home : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
     private lateinit var videoAdapter: videoAdapter
-    private lateinit var bookAdapter: bookAdapter
+    private lateinit var bookAdapter: BooksAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,14 +48,17 @@ class Home : Fragment() {
         val viewModelFactory = HomeViewModelFactory(newsRepository)
         viewModel = ViewModelProvider(this, viewModelFactory)[HomeViewModel::class.java]
 
-        bookAdapter = bookAdapter()
+        bookAdapter = BooksAdapter(viewModel)
 
         binding.homeRvAllbooks.apply {
             adapter = bookAdapter
             layoutManager = LinearLayoutManager(activity)
         }
 
-
+        viewModel._trendingBooks?.observe(viewLifecycleOwner) {
+            Log.d("final result search", it.toString())
+            bookAdapter.differ.submitList(it)
+        }
 
         return root
     }
